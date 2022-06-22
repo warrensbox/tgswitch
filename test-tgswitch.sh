723b3eec5dcd98f05ec03a25f6679bb392ba9a8b
warrenbox <warren.veerasingam@gmail.com>
2022-06-22 15:28:49 -0500

    added test script

--- test-tgswitch.sh
+++ test-tgswitch.sh
@@ -8,7 +8,15 @@ fi
 #find ./test-data/* -type d -print0 | while read -r -d $'\0' TEST_PATH;do ./build/tgswitch -c "${TEST_PATH}" || exit 1; done;
 
 echo "Test terragrunt-version file"
-./build/tgswitch -c ./test-data/test_terragrunt-version
+./build/tgswitch -c ./test-data/test_terragrunt-version || exit 1
+version=$(terragrunt -v | awk '{print $3}')
+echo "$version"
+if [[ "$version" == "v0.36.0" ]]; then
+    echo "Switch successful"
+else
+    echo "Switch failed"
+    exit 1
+fi
 
 
 echo "Test terragrunt hcl file"
@@ -18,4 +26,6 @@ echo "Test tfswitchrc file"
 
 
 echo "Test tfswitchtoml file"
+
+echo "Test environment variable"
 
