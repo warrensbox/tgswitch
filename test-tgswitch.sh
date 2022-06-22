348e7b40105bd6a11157a1384d1a7c7767b7cde9
warrenbox <warren.veerasingam@gmail.com>
2022-06-22 15:20:09 -0500

    added test file

--- test-tgswitch.sh
+++ test-tgswitch.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
 
-find ./test-data/* -type d -print0 | while read -r -d $'\0' TEST_PATH;do ./build/tgswitch -c "${TEST_PATH}" || exit 1; done;
+if [[ ! -f "./build/tgswitch" ]]; then
+    echo "Cannot run test.."
+    echo "Build does not exist"
+    exit 1
+fi
+#find ./test-data/* -type d -print0 | while read -r -d $'\0' TEST_PATH;do ./build/tgswitch -c "${TEST_PATH}" || exit 1; done;
+
+echo "Test terragrunt-version file"
+./build/tgswitch -c ./test-data/test_terragrunt-version
+
+
+echo "Test terragrunt hcl file"
+
+
+echo "Test tfswitchrc file"
+
+
+echo "Test tfswitchtoml file"
 
