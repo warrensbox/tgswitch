af9d530f3738f90f6e1fb5ab6de7ad422fbba82d
warrenbox <warren.veerasingam@gmail.com>
2022-06-22 16:09:26 -0500

    final test

--- test-tgswitch.sh
+++ test-tgswitch.sh
@@ -7,7 +7,17 @@ if [[ ! -f "./build/tgswitch" ]]; then
 fi
 #find ./test-data/* -type d -print0 | while read -r -d $'\0' TEST_PATH;do ./build/tgswitch -c "${TEST_PATH}" || exit 1; done;
 
+function commentStart(){
+    echo "### Test start ###"
+}
+
+function commentComplete(){
+    echo "### Test complete ###" 
+    echo "" 
+}
+
 function runtestdir(){
+    commentStart
     test_case=$1
     dir=$2
     expected_version=$3
@@ -21,10 +31,12 @@ function runtestdir(){
         echo "Switch failed"
         exit 1
     fi
+    commentComplete
 }
 
 
 function runtestenv(){
+    commentStart
     test_case=$1
     env=$2
     expected_version=$3
@@ -40,6 +52,25 @@ function runtestenv(){
         echo "Switch failed"
         exit 1
     fi
+    commentComplete
+}
+
+function runtestarg(){
+    commentStart
+    test_case=$1
+    arg=$2
+    expected_version=$3
+    echo "Test $test_case"
+    ./build/tgswitch "$arg"|| exit 1
+    version=$(terragrunt -v | awk '{print $3}')
+    echo "$version"
+    if [[ "$version" == "$expected_version" ]]; then
+        echo "Switch successful"
+    else
+        echo "Switch failed"
+        exit 1
+    fi
+    commentComplete
 }
 
 runtestdir "terragrunt version" "test_terragrunt-version" "v0.36.0"
@@ -47,4 +78,4 @@ runtestdir "terragrunt hcl" "test_terragrunt_hcl" "v0.37.4"
 runtestdir "tfswitchrc" "test_tgswitchrc" "v0.33.0"
 runtestdir ".toml" "test_tfswitchtoml" "v0.34.0"
 runtestenv "env variable" "0.37.1" "v0.37.1"
-
+runtestarg "passing argument" "0.36.1" "v0.36.1"
